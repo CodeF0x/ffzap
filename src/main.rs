218fc3ee3a16038954f2e14bc8a478af8f4f960a
CodeF0x <contact@codef0x.dev>
2024-10-15 03:00:49 +0200

    feat: proper output file format via options and proper options passthrough to ffmpeg

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use clap::Parser;
-use rand::random;
 use std::fs::read_dir;
 use std::path::{Path, PathBuf};
 use std::process::{Command, Stdio};
 use std::sync::{Arc, Mutex};
 use std::{io, thread};
 
-#[derive(Parser, Debug)]
+#[derive(Parser, Debug, Clone)]
 #[command(version, about)]
 struct CmdArgs {
     #[arg(short, long, default_value_t = 2)]
@@ -20,12 +19,14 @@ struct CmdArgs {
 
     #[arg(short, long)]
     output_dir: Option<String>,
+
+    #[arg(short, long)]
+    name_scheme: Option<String>,
 }
 
 fn main() -> io::Result<()> {
     let cmd_args = CmdArgs::parse();
 
-    // mtc name
     let paths = Arc::new(Mutex::new(
         read_dir(Path::new(&cmd_args.source_dir))?
             .map(|res| res.map(|e| e.path()))
@@ -36,7 +37,7 @@ fn main() -> io::Result<()> {
 
     for _ in 0..cmd_args.thread_count {
         let paths = Arc::clone(&paths);
-        let options = cmd_args.ffmpeg_options.clone();
+        let args = cmd_args.clone();
 
         let handle = thread::spawn(move || loop {
             let path_to_process = {
@@ -48,24 +49,36 @@ fn main() -> io::Result<()> {
             match path_to_process {
                 Some(path) => {
                     println!("Processing {:?}", path);
+                    let split_options = &mut args.ffmpeg_options.split(' ').collect::<Vec<&str>>();
+                    let file_extension_from_options = split_options[split_options.len() - 1]
+                        .split('.')
+                        .last()
+                        .unwrap();
+                    let mut file_name = format!(
+                        "{}_transcoded.{}",
+                        path.file_stem().unwrap().to_str().unwrap(),
+                        file_extension_from_options
+                    );
+                    // remove both file stem and file format to just get the options
+                    split_options.pop();
+
+                    if let Some(_scheme) = &args.name_scheme {
+                        file_name = String::from("");
+                    }
+
                     let output = Command::new("ffmpeg")
                         .args(["-i", path.to_str().unwrap()])
-                        .args(options.split(' ').collect::<Vec<&str>>())
-                        .arg(format!("{}.mp4", random::<u8>()))
+                        .args(split_options)
+                        .arg(file_name)
                         .stdout(Stdio::piped())
                         .stderr(Stdio::piped())
                         .output()
                         .unwrap();
-                    if !output.stderr.is_empty() {
+                    if !output.status.success() {
                         println!("Error!");
                         println!("Error is: {}", String::from_utf8(output.stderr).unwrap());
+                        break;
                     }
-                    // let output = Command::new("ffmpeg")
-                    //     .args(["--help"])
-                    //     .stdout(Stdio::piped())
-                    //     .output()
-                    //     .unwrap();
-                    // println!("{}", String::from_utf8(output.stdout).unwrap());
                 }
                 None => {
                     break;
