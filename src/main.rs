6c16f1aac891ebd5490ce618bcb001cb2cba1ecc
CodeF0x <contact@codef0x.dev>
2024-10-17 18:20:37 +0200

    feat: output name templating syntax

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,41 @@
 use clap::Parser;
 use glob::glob;
-use std::path::PathBuf;
+use std::ffi::OsStr;
+use std::fs::create_dir_all;
+use std::path::{Path, PathBuf};
 use std::process::{Command, Stdio};
 use std::sync::{Arc, Mutex};
-use std::{io, thread};
+use std::thread;
 
 #[derive(Parser, Debug, Clone)]
 #[command(version, about)]
 struct CmdArgs {
+    /// the amount of threads you want to utilize. most systems can handle 2. go higher if you have a powerful computer.
     #[arg(short, long, default_value_t = 2)]
     thread_count: u8,
 
+    /// options you want to pass to ffmpeg. for the output file name, use --output
     #[arg(short, long, allow_hyphen_values = true)]
     ffmpeg_options: String,
 
+    /// the directory with all files you want to process. supports unix globs
     #[arg(short, long)]
     input_directory: String,
 
+    /// Specify the output file pattern. Use placeholders to customize file paths:
+    ///
+    /// {{dir}}  - Original file's directory structure
+    ///
+    /// {{name}} - Original file's name (without extension)
+    ///
+    /// {{ext}}  - Original file's extension
+    ///
+    /// Example: /destination/{{dir}}/{{name}}_transcoded.{{ext}}
+    ///
+    /// Outputs the file in /destination, mirroring the original structure and keeping the file extension.
     #[arg(short, long)]
-    output_directory: Option<String>,
-
-    #[arg(short, long)]
-    name_scheme: Option<String>,
+    output: String,
+    // {{ext}} -> extension, {{name}} filename without extension, {{dir}} -> directory structure from starting point to file, {{parent}} -> parent directory of starting point
 }
 
 fn main() {
@@ -52,34 +66,58 @@ fn main() {
                 Some(path) => {
                     println!("Processing {}", path.display());
                     let split_options = &mut args.ffmpeg_options.split(' ').collect::<Vec<&str>>();
-                    let file_extension_from_options = split_options[split_options.len() - 1]
-                        .split('.')
-                        .last()
-                        .unwrap();
-                    let mut file_name = format!(
-                        "{}_transcoded.{}",
-                        path.file_stem().unwrap().to_str().unwrap(),
-                        file_extension_from_options
-                    );
-                    // remove both file stem and file format to just get the options
-                    split_options.pop();
 
-                    if let Some(_scheme) = &args.name_scheme {
-                        file_name = String::from("");
+                    let mut final_file_name = args
+                        .output
+                        .replace("{{ext}}", path.extension().unwrap().to_str().unwrap());
+                    final_file_name = final_file_name
+                        .replace("{{name}}", &path.file_stem().unwrap().to_str().unwrap());
+                    final_file_name = final_file_name.replace(
+                        "{{dir}}",
+                        &path.parent().unwrap_or(Path::new("")).to_str().unwrap(),
+                    );
+                    final_file_name = final_file_name.replace(
+                        "{{parent}}",
+                        &path
+                            .parent()
+                            .unwrap_or(Path::new(""))
+                            .file_name()
+                            .unwrap_or(OsStr::new(""))
+                            .to_str()
+                            .unwrap_or(""),
+                    );
+                    let final_path_parent = Path::new(&final_file_name).parent().unwrap();
+
+                    if !final_path_parent.exists() {
+                        match create_dir_all(final_path_parent) {
+                            Ok(_) => {}
+                            Err(err) => {
+                                eprintln!(
+                                    "Could not create directory structure for file {}",
+                                    final_file_name
+                                );
+                                eprintln!("{}", err)
+                            }
+                        }
                     }
 
-                    let output = Command::new("ffmpeg")
+                    if let Ok(output) = Command::new("ffmpeg")
                         .args(["-i", path.to_str().unwrap()])
                         .args(split_options)
-                        .arg(file_name)
+                        .arg(&final_file_name)
                         .stdout(Stdio::piped())
                         .stderr(Stdio::piped())
                         .output()
-                        .unwrap();
-                    if !output.status.success() {
-                        eprintln!("Error!");
-                        eprintln!("Error is: {}", String::from_utf8_lossy(&output.stderr));
-                        break;
+                    {
+                        if output.status.success() {
+                            println!("Success, saving to {final_file_name}");
+                        } else {
+                            eprintln!("Error!");
+                            eprintln!("Error is: {}", String::from_utf8_lossy(&output.stderr));
+                            eprintln!("Continuing with next task if there's more to do...");
+                        }
+                    } else {
+                        eprintln!("There was an error running ffmpeg. Please check it's correctly installed and working as intended.");
                     }
                 }
                 None => {
