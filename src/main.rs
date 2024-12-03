d57150706410e6184ff119f3bac98a62dd53bda1
CodeF0x <contact@codef0x.dev>
2024-12-03 18:01:50 +0100

    feat: multiprogress enabling correct formatting

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
+mod progress;
+
+use crate::progress::Progress;
 use clap::Parser;
-use indicatif::{ProgressBar, ProgressStyle};
 use std::ffi::OsStr;
 use std::fs::create_dir_all;
 use std::path::Path;
 use std::process::{Command, Stdio};
 use std::sync::{Arc, Mutex};
 use std::thread;
-use std::time::Duration;
 
 #[derive(Parser, Debug, Clone)]
 #[command(version, about)]
@@ -42,20 +43,16 @@ struct CmdArgs {
 fn main() {
     let cmd_args = CmdArgs::parse();
 
-    let progress = ProgressBar::new(cmd_args.input_directory.len() as u64);
-    progress.set_style(
-        ProgressStyle::default_bar()
-            .template("{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {pos}/{len}")
-            .expect("Failed to set progress style")
-            .progress_chars("#>-"),
-    );
+    let progress = Arc::new(Progress::new(cmd_args.input_directory.len()));
+    progress.start_stick(500);
+
     let paths = Arc::new(Mutex::new(cmd_args.input_directory));
 
     let mut thread_handles = vec![];
 
     for thread in 0..cmd_args.thread_count {
         let paths = Arc::clone(&paths);
-        let progress = progress.clone();
+        let progress = Arc::clone(&progress);
         let ffmpeg_options = cmd_args.ffmpeg_options.clone();
         let output = cmd_args.output.clone();
 
@@ -70,14 +67,18 @@ fn main() {
                     let path = Path::new(&path);
 
                     if !path.is_file() {
-                        eprintln!(
+                        progress.println(format!(
                             "[THREAD {thread}] -- {} doesn't appear to be a file, ignoring. Continuing with next task if there's more to do...",
                             path.to_str().unwrap()
-                        );
+                        ));
                         continue;
                     }
 
-                    println!("[THREAD {thread}] -- Processing {}", path.display());
+                    progress.println(format!(
+                        "[THREAD {thread}] -- Processing {}",
+                        path.display()
+                    ));
+
                     let split_options = &mut ffmpeg_options.split(' ').collect::<Vec<&str>>();
 
                     let mut final_file_name =
@@ -104,10 +105,10 @@ fn main() {
                         match create_dir_all(final_path_parent) {
                             Ok(_) => {}
                             Err(err) => {
-                                eprintln!(
-                                    "[THREAD {thread}] -- Could not create directory structure for file {}",
+                                progress.println(
+                                    format!("[THREAD {thread}] -- Could not create directory structure for file {}",
                                     final_file_name
-                                );
+                                ));
                                 eprintln!("{}", err)
                             }
                         }
@@ -122,14 +123,16 @@ fn main() {
                         .output()
                     {
                         if output.status.success() {
-                            println!("[THREAD {thread}] -- Success, saving to {final_file_name}");
+                            progress.println(format!(
+                                "[THREAD {thread}] -- Success, saving to {final_file_name}"
+                            ));
                         } else {
-                            eprintln!("[THREAD {thread}] -- Error!");
-                            eprintln!(
+                            progress.println(format!("[THREAD {thread}] -- Error!"));
+                            progress.println(format!(
                                 "[THREAD {thread}] -- Error is: {}",
                                 String::from_utf8_lossy(&output.stderr)
-                            );
-                            eprintln!("[THREAD {thread}] -- Continuing with next task if there's more to do...");
+                            ));
+                            progress.println(format!("[THREAD {thread}] -- Continuing with next task if there's more to do..."));
                         }
 
                         progress.inc(1);
@@ -146,15 +149,9 @@ fn main() {
         thread_handles.push(handle);
     }
 
-    thread::spawn(move || {
-        while !progress.is_finished() {
-            progress.tick();
-            thread::sleep(Duration::new(0, 500));
-        }
-        progress.finish();
-    });
-
     for handle in thread_handles {
         handle.join().unwrap();
     }
+
+    progress.finish();
 }
