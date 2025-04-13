f1e46d0eee0913ab3572e703f6a9df65a9a60977
CodeF0x <contact@codef0x.dev>
2025-04-14 00:59:27 +0200

    feat: added file list

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,8 @@ use std::thread;
 #[derive(Debug, Clone)]
 pub enum Message {
     Start,
-    EditInputFiles(text_editor::Action),
+    EditInputFiles(String),
+    EditFileList(String),
     EditFfmpegOption(text_editor::Action),
     EditOutputPattern(String),
     EditThreadAmount(u16),
@@ -20,8 +21,8 @@ pub enum Message {
 }
 
 struct Gui {
-    input_files: text_editor::Content,
-    file_list: Option<String>,
+    input_files: String,
+    file_list: String,
     output_pattern: String,
     threads: u16,
     ffmpeg_options: text_editor::Content,
@@ -32,22 +33,30 @@ struct Gui {
 
 impl Gui {
     pub fn view(&self) -> Column<Message> {
-        let input_files = text_editor(&self.input_files)
-            .placeholder(
-                "Enter the file paths of the files you want to process, seperated by comma",
-            )
-            .on_action(Message::EditInputFiles);
+        let mut input_files = text_input("Enter the file paths of the files you want to process, seperated by space",&self.input_files);
+        let mut file_list = text_input("Enter path to file list", &self.file_list);
+        
         let ffmpeg_options = text_editor(&self.ffmpeg_options)
             .placeholder("Enter the ffmpeg_options you want to process")
             .on_action(Message::EditFfmpegOption);
+        
         let output_pattern = text_input("Enter your output pattern", &self.output_pattern)
             .on_input(Message::EditOutputPattern);
-        let thread_input = number_input(&self.threads, 1..=100, Message::EditThreadAmount).on_input(Message::EditThreadAmount);
+        
+        let thread_input = number_input(&self.threads, 1..=u16::MAX, Message::EditThreadAmount).on_input(Message::EditThreadAmount);
         let delete = checkbox("Delete source files after job successfully finished", self.delete).on_toggle(Message::ToggleDelete);
         let overwrite = checkbox("Overwrite target if it already exists", self.overwrite).on_toggle(Message::ToggleOverwrite);
         
+        if self.input_files.is_empty() {
+            file_list = file_list.on_input(Message::EditFileList);
+        }
+        if self.file_list.is_empty() {
+            input_files = input_files.on_input(Message::EditInputFiles);
+        }
+        
         column![
             input_files,
+            file_list,
             ffmpeg_options,
             output_pattern,
             thread_input,
@@ -59,14 +68,17 @@ impl Gui {
 
     pub fn update(&mut self, message: Message) {
         match message {
-            Message::EditInputFiles(action) => {
-                self.input_files.perform(action);
+            Message::EditInputFiles(files) => {
+                self.input_files = files;
+            }
+            Message::EditFileList(list) => {
+                self.file_list = list;
             }
             Message::EditFfmpegOption(action) => {
                 self.ffmpeg_options.perform(action);
             }
-            Message::EditOutputPattern(output) => {
-                self.output_pattern = output;
+            Message::EditOutputPattern(pattern) => {
+                self.output_pattern = pattern;
             }
             Message::EditThreadAmount(amount_of_threads) => {
                 self.threads = amount_of_threads;
@@ -76,19 +88,22 @@ impl Gui {
             }
             Message::ToggleOverwrite(overwrite) => {
                 self.overwrite = overwrite;
-                println!("{overwrite}");
             }
             Message::Start => {
-                let input_file_paths = if self.input_files.text().is_empty() {
+                let input = if self.input_files.trim().is_empty() {
                     None
                 } else {
                     Some(
                         self.input_files
-                            .text()
                             .split(' ')
                             .map(|s| s.trim().to_string())
                             .collect::<Vec<String>>(),
                     )
+                };
+                let file_list = if self.file_list.trim().is_empty() {
+                    None
+                } else {
+                    Some(self.file_list.clone())
                 };
                 let ffmpeg_options = if self.ffmpeg_options.text().trim().is_empty() {
                     None
@@ -105,8 +120,8 @@ impl Gui {
                     job::run_job(CmdArgs {
                         thread_count,
                         ffmpeg_options,
-                        input: input_file_paths,
-                        file_list: None,
+                        input,
+                        file_list,
                         overwrite,
                         verbose: true,
                         delete,
