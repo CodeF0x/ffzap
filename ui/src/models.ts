1b1dd0adcc6b83a7b924443868dc307c51b61e4e
CodeF0x <contact@codef0x.dev>
2025-07-17 22:17:23 +0200

    feat: add frontend -> backend communicaton

---
+++ ui/src/models.ts
@@ -0,0 +1,12 @@
+// For more information on what these properties do, see shared/src/args.rs
+export interface CmdArgs {
+    thread_count: number;
+    ffmpeg_options?: string | null;
+    input?: string[] | null;
+    file_list?: string | null;
+    overwrite: boolean;
+    verbose: boolean;
+    delete: boolean;
+    eta: boolean;
+    output: string;
+}
