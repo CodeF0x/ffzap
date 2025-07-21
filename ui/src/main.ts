e940bfca9bf0ab61449c4a235b9b9c058dd08448
CodeF0x <contact@codef0x.dev>
2025-07-21 22:29:50 +0200

    chore: prettier formatting

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -2,113 +2,152 @@ import { open } from '@tauri-apps/plugin-dialog';
 import { invoke } from '@tauri-apps/api/core';
 import { CmdArgs, LogSeverity } from './models';
 import { listen } from '@tauri-apps/api/event';
-import { updateFileCount, updatePathsList, updateFileList, updateProgressBar, showProgressBar, showLogSection, clearLogSection, addSpacerToLog, updateLog, prepareTabs } from './dom';
+import {
+  updateFileCount,
+  updatePathsList,
+  updateFileList,
+  updateProgressBar,
+  showProgressBar,
+  showLogSection,
+  clearLogSection,
+  addSpacerToLog,
+  updateLog,
+  prepareTabs,
+} from './dom';
 
 document.addEventListener('DOMContentLoaded', () => {
-    let allFiles: string[] | null = null;
-    let filesList: string | null = null;
-    let totalFiles: number = 0;
-    let doneFiles: number = 0;
+  let allFiles: string[] | null = null;
+  let filesList: string | null = null;
+  let totalFiles: number = 0;
+  let doneFiles: number = 0;
 
-    prepareTabs();
+  prepareTabs();
 
-    document.getElementById('browse-files-btn')!.addEventListener('click', async () => {
-        const files: string[] | null = await open({
-            multiple: true,
-            directory: false,
-        });
+  document
+    .getElementById('browse-files-btn')!
+    .addEventListener('click', async () => {
+      const files: string[] | null = await open({
+        multiple: true,
+        directory: false,
+      });
 
-        allFiles = files;
-        filesList = null;
+      allFiles = files;
+      filesList = null;
 
-        updateFileCount(allFiles?.length ?? 0);
-        updatePathsList(allFiles);
-        updateFileList(filesList);
+      updateFileCount(allFiles?.length ?? 0);
+      updatePathsList(allFiles);
+      updateFileList(filesList);
     });
-    document.getElementById('browse-list-btn')!.addEventListener('click', async () => {
-        const file: string | null = await open({
-            multiple: false,
-            directory: false,
-        });
+  document
+    .getElementById('browse-list-btn')!
+    .addEventListener('click', async () => {
+      const file: string | null = await open({
+        multiple: false,
+        directory: false,
+      });
 
-        allFiles = [];
-        filesList = file;
+      allFiles = [];
+      filesList = file;
 
-        updateFileCount(0);
-        updatePathsList(allFiles);
-        updateFileList(filesList);
-    });
-
-    document.getElementById('start-btn')!.addEventListener('click', event => {
-        const startBtn: HTMLButtonElement = event.target as HTMLButtonElement;
-        const threadCountInput: HTMLInputElement = document.getElementById('thread-count')! as HTMLInputElement;
-        const ffmpegOptionsInput: HTMLTextAreaElement = document.getElementById('ffmpeg-options')! as HTMLTextAreaElement;
-        const outputPatternInput: HTMLInputElement = document.getElementById('output-pattern')! as HTMLInputElement;
-        const overwriteCheckbox: HTMLInputElement = document.getElementById('overwrite')! as HTMLInputElement;
-        const verboseCheckbox: HTMLInputElement = document.getElementById('verbose')! as HTMLInputElement;
-        const deleteCheckbox: HTMLInputElement = document.getElementById('delete-source')! as HTMLInputElement;
-
-        const args: CmdArgs = {
-            thread_count: Number(threadCountInput.value),
-            ffmpeg_options: ffmpegOptionsInput.value ? ffmpegOptionsInput.value : null,
-            input: allFiles ? allFiles : null,
-            file_list: filesList ? filesList : null,
-            overwrite: overwriteCheckbox.checked,
-            verbose: verboseCheckbox.checked,
-            delete: deleteCheckbox.checked,
-            eta: false,
-            output: outputPatternInput.value
-        };
-
-        doneFiles = 0;
-
-        updateProgressBar(doneFiles, totalFiles);
-        showProgressBar();
-        showLogSection(verboseCheckbox.checked);
-        clearLogSection();
-
-        invoke('start_job', { options: JSON.stringify(args) });
-        startBtn.disabled = true;
+      updateFileCount(0);
+      updatePathsList(allFiles);
+      updateFileList(filesList);
     });
 
-    listen<[string, number, string[]]>('job-finished', event => {
-        (document.getElementById('start-btn') as HTMLButtonElement).disabled = false;
+  document.getElementById('start-btn')!.addEventListener('click', event => {
+    const startBtn: HTMLButtonElement = event.target as HTMLButtonElement;
+    const threadCountInput: HTMLInputElement = document.getElementById(
+      'thread-count'
+    )! as HTMLInputElement;
+    const ffmpegOptionsInput: HTMLTextAreaElement = document.getElementById(
+      'ffmpeg-options'
+    )! as HTMLTextAreaElement;
+    const outputPatternInput: HTMLInputElement = document.getElementById(
+      'output-pattern'
+    )! as HTMLInputElement;
+    const overwriteCheckbox: HTMLInputElement = document.getElementById(
+      'overwrite'
+    )! as HTMLInputElement;
+    const verboseCheckbox: HTMLInputElement = document.getElementById(
+      'verbose'
+    )! as HTMLInputElement;
+    const deleteCheckbox: HTMLInputElement = document.getElementById(
+      'delete-source'
+    )! as HTMLInputElement;
 
-        addSpacerToLog();
+    const args: CmdArgs = {
+      thread_count: Number(threadCountInput.value),
+      ffmpeg_options: ffmpegOptionsInput.value
+        ? ffmpegOptionsInput.value
+        : null,
+      input: allFiles ? allFiles : null,
+      file_list: filesList ? filesList : null,
+      overwrite: overwriteCheckbox.checked,
+      verbose: verboseCheckbox.checked,
+      delete: deleteCheckbox.checked,
+      eta: false,
+      output: outputPatternInput.value,
+    };
 
-        const successfulFiles: number = event.payload[1];
-        const logLine: string = `${successfulFiles} out of ${totalFiles} files have been successful. A detailed log has been written to ${event.payload[0]}`;
-        updateLog(logLine, totalFiles !== successfulFiles ? LogSeverity.ERROR : LogSeverity.INFO);
+    doneFiles = 0;
 
-        const failedPaths: string[] = event.payload[2];
-        if (failedPaths.length > 0) {
-            addSpacerToLog();
+    updateProgressBar(doneFiles, totalFiles);
+    showProgressBar();
+    showLogSection(verboseCheckbox.checked);
+    clearLogSection();
 
-            const staticLine: string = 'The following files were not processed due to the errors above:';
-            const failedPathsList: string = failedPaths.join('\n');
-            const finalLine: string = `${staticLine}\n${failedPathsList}`;
+    invoke('start_job', { options: JSON.stringify(args) });
+    startBtn.disabled = true;
+  });
 
-            updateLog(finalLine, LogSeverity.ERROR);
-        }
-    });
+  listen<[string, number, string[]]>('job-finished', event => {
+    (document.getElementById('start-btn') as HTMLButtonElement).disabled =
+      false;
 
-    listen<number>('update-total-file-count', event => totalFiles = event.payload);
+    addSpacerToLog();
 
-    listen<number>('progress-update', event => {
-        doneFiles = event.payload;
-        updateProgressBar(doneFiles, totalFiles);
-    });
+    const successfulFiles: number = event.payload[1];
+    const logLine: string = `${successfulFiles} out of ${totalFiles} files have been successful. A detailed log has been written to ${event.payload[0]}`;
+    updateLog(
+      logLine,
+      totalFiles !== successfulFiles ? LogSeverity.ERROR : LogSeverity.INFO
+    );
 
-    listen<string>('log-update-info', event => {
-        updateLog(event.payload, LogSeverity.INFO);
-    });
+    const failedPaths: string[] = event.payload[2];
+    if (failedPaths.length > 0) {
+      addSpacerToLog();
 
-    listen<string>('log-update-error', event => {
-        updateLog(event.payload, LogSeverity.ERROR);
-    });
+      const staticLine: string =
+        'The following files were not processed due to the errors above:';
+      const failedPathsList: string = failedPaths.join('\n');
+      const finalLine: string = `${staticLine}\n${failedPathsList}`;
 
-    listen('job-finished', () => {
-        const startBtn: HTMLButtonElement = document.getElementById('start-btn')! as HTMLButtonElement;
-        startBtn.disabled = false;
-    });
+      updateLog(finalLine, LogSeverity.ERROR);
+    }
+  });
+
+  listen<number>(
+    'update-total-file-count',
+    event => (totalFiles = event.payload)
+  );
+
+  listen<number>('progress-update', event => {
+    doneFiles = event.payload;
+    updateProgressBar(doneFiles, totalFiles);
+  });
+
+  listen<string>('log-update-info', event => {
+    updateLog(event.payload, LogSeverity.INFO);
+  });
+
+  listen<string>('log-update-error', event => {
+    updateLog(event.payload, LogSeverity.ERROR);
+  });
+
+  listen('job-finished', () => {
+    const startBtn: HTMLButtonElement = document.getElementById(
+      'start-btn'
+    )! as HTMLButtonElement;
+    startBtn.disabled = false;
+  });
 });
--- ui/src/models.ts
+++ ui/src/models.ts
@@ -1,17 +1,17 @@
 // For more information on what these properties do, see shared/src/args.rs
 export interface CmdArgs {
-    thread_count: number;
-    ffmpeg_options?: string | null;
-    input?: string[] | null;
-    file_list?: string | null;
-    overwrite: boolean;
-    verbose: boolean;
-    delete: boolean;
-    eta: boolean;
-    output: string;
+  thread_count: number;
+  ffmpeg_options?: string | null;
+  input?: string[] | null;
+  file_list?: string | null;
+  overwrite: boolean;
+  verbose: boolean;
+  delete: boolean;
+  eta: boolean;
+  output: string;
 }
 
 export enum LogSeverity {
-    ERROR = 'error',
-    INFO = 'info'
+  ERROR = 'error',
+  INFO = 'info',
 }
